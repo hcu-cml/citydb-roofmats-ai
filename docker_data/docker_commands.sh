62505e5fe0b4a3b283089bf540634dba199f6c45
Son-HNguyen <son.nguyen@hcu-hamburg.de>
2025-08-29 17:09:24 +0700

    Add Docker image for Windows

--- docker_data/docker_commands.sh
+++ docker_data/docker_commands.sh
@@ -27,3 +27,20 @@ docker buildx build \
   --platform linux/amd64,linux/arm64 \
   -t sonhng/citydb-roofmats-ai:linux-latest \
   --push .
+
+# ----
+
+# FOR WINDOWS
+
+# FOR LOCAL ARCHITECTURE
+
+# Build Docker image
+docker build -t citydb-roofmats-ai .
+
+# Run the Docker container
+docker run -p 8080:80 -p 5000:5000 citydb-roofmats-ai
+
+# Push to DockerHub
+docker tag citydb-roofmats-ai sonhng/citydb-roofmats-ai:windows-latest
+docker login
+docker push sonhng/citydb-roofmats-ai:windows-latest
